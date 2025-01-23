862eb2274777e5ae7c52883e06604ad6d3b42e1c
Gennadiy <renardes@gmail.com>
2025-01-23 22:05:21 +0500

    аргумент пути к файлу

--- bd/management/commands/import_data.py
+++ bd/management/commands/import_data.py
@@ -1,10 +1,17 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import connection
 
+from pathlib import Path
+
 
 class Command(BaseCommand):
-    help = 'Импорт данных из SQL файла'
-
+    help = 'чтение SQL скрипта из файла и выполнение INSERT запросов'
+    
+    def add_arguments(self, parser):
+        parser.add_argument('sql_script_path', type=Path)
+    
     def handle(self, *args, **options):
+        sql_script_path = settings.BASE_DIR / options['sql_script_path']
         
 

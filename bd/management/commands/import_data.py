4e2984e36e509739e32196aae1d66359e104af72
Gennadiy <renardes@gmail.com>
2025-01-23 22:25:10 +0500

    получение списка SQL запросов

--- bd/management/commands/import_data.py
+++ bd/management/commands/import_data.py
@@ -3,15 +3,23 @@ from django.core.management.base import BaseCommand
 from django.db import connection
 
 from pathlib import Path
-
+from re import compile
 
 class Command(BaseCommand):
     help = 'чтение SQL скрипта из файла и выполнение INSERT запросов'
+    pat_query_sep = compile(r';\n*')
     
     def add_arguments(self, parser):
         parser.add_argument('sql_script_path', type=Path)
     
     def handle(self, *args, **options):
         sql_script_path = settings.BASE_DIR / options['sql_script_path']
+        sql_script = sql_script_path.read_text(encoding='utf-8')
+        
+        querries = self.pat_query_sep.split(sql_script)
+        try:
+            querries.remove('')
+        except ValueError:
+            pass
         
 
